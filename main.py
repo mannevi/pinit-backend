23378911d01a1368d6c74af9fe13d1f99d3c901c
Manne Vaishnavi <mannevaishnavi19@gmail.com>
2026-03-01 12:36:32 -0500

    Allow all origins temporarily

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS — allow React frontend to call this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["http://localhost:3000"],
+    allow_origins     = ["*"],
     allow_credentials =True,
     allow_methods     = ["*"],
     allow_headers     = ["*"]
